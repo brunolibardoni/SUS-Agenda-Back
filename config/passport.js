625aed45a0d44bd894e3d7f44df4f6003ec37ee0
Bruno Libardoni <brunolibardoni16@gmail.com>
2025-09-13 20:35:10 -0300

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -32,7 +32,7 @@ async function checkProfileComplete(userId) {
 }
 
 passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientID: process.env.GOOGLE_CLIENT_ID, 
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3002/auth/google/callback'
 },
