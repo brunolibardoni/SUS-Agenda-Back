ca1cb2f506078e3cf4e136afe1751a75d692b47f
Bruno Libardoni <brunolibardoni16@gmail.com>
2025-09-15 15:06:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cookieParser()); // Parse cookies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//app.set('trust proxy', 1);
+app.set('trust proxy', 1);
 
 // Session middleware
 app.use(session({
