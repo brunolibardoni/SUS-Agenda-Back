a94fcb80b97cd601bde72cc98ed27dd02a3bfb22
Bruno Libardoni <brunolibardoni16@gmail.com>
2025-09-16 09:17:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -58,6 +58,17 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
+// CORS middleware para permitir cookies cross-site
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://nice-moss-0eff7d51e.1.azurestaticapps.net'); // Troque pelo domínio do frontend
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(204);
+  } else {
+    next();
+  }
+});
 app.use(cookieParser()); // Parse cookies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
